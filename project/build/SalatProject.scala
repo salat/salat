0637638ec86b2219f4c0f71b0e67256b606e1f75
rktoomey <rose@novus.com>
2011-01-13 23:20:11 -0500

    Added withSources() to Salat project.

--- project/build/SalatProject.scala
+++ project/build/SalatProject.scala
@@ -5,22 +5,22 @@ class SalatProject(info: ProjectInfo) extends DefaultProject(info) with posterou
 
   override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
 
-  val mongodb = "org.mongodb" % "mongo-java-driver" % "2.4"
-  val casbah_core = "com.mongodb.casbah" %% "casbah-core" % "2.0.1"
+  val mongodb = "org.mongodb" % "mongo-java-driver" % "2.4" withSources()
+  val casbah_core = "com.mongodb.casbah" %% "casbah-core" % "2.0.1" withSources()
 
   // Should be crossScalaVersionString, but 2.8.0's scalap appears to
   // be totally frakked, whereas 2.8.1's works fine with 2.8.0. Go
   // figure.
-  val scalap = "org.scala-lang" % "scalap" % "2.8.1"
+  val scalap = "org.scala-lang" % "scalap" % "2.8.1" withSources()
 
   val specsVersion = crossScalaVersionString match {
     case "2.8.0" => "1.6.5"
     case "2.8.1" => "1.6.6"
   }
-  val specs = "org.scala-tools.testing" %% "specs" % specsVersion % "test->default"
+  val specs = "org.scala-tools.testing" %% "specs" % specsVersion % "test->default" withSources()
 
-  val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default"
-  val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default"
+  val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default" withSources()
+  val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default" withSources()
 
   val publishTo = Resolver.sftp("repo.novus.com", "repo.novus.com", "/nv/repo/%s".format(
     if (projectVersion.value.toString.endsWith("-SNAPSHOT")) "snapshots"
