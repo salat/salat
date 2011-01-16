f567ff57d45d215b78daa8d372275779e1e7c4fd
Jeff Sack <jeff.sack@gmail.com>
2011-01-16 01:28:33 -0500

    modified spec to demonstrate that prior serialization calls eliminate 'no grater found' errors.

--- src/test/scala/com/novus/salat/test/SalatTraitsCollectionsSpec.scala
+++ src/test/scala/com/novus/salat/test/SalatTraitsCollectionsSpec.scala
@@ -9,92 +9,59 @@ import com.mongodb.casbah.Imports._
 
 class SalatTraitsCollectionsSpec extends SalatSpec {
 
-  // TODO: create an issue for this?
-
   "a grater" should {
-//    "properly serialize/deserialize a list of objects implementing a common trait" in {
-//
-//      val obj = SomeTopLevelClass(true,
-//        SomeSecondLevelClass("b",
-//          SomeContainerClass("1",
-//            List(SomeSubclassExtendingSaidTrait(2),
-//              AnotherSubclassExtendingSaidTrait(3.0),
-//              SomeSubclassExtendingSaidTrait(4),
-//              AnotherSubclassExtendingSaidTrait(5.0)))))
-//
-//
-//      val dbo: MongoDBObject = grater[SomeTopLevelClass].asDBObject(obj)
-//
-//      dbo must havePair("a" -> obj.a)
-//
-//      val secondLevelDbo: MongoDBObject = dbo.expand[BasicDBObject]("b").get
-//
-//      secondLevelDbo must havePair("c" -> obj.b.c)
-//
-//      val containerDbo: MongoDBObject = secondLevelDbo.expand[BasicDBObject]("d").get
-//      val containerObj: SomeContainerClass = obj.b.d
-//
-//      containerDbo must havePair("e" -> containerObj.e)
-//
-//      val listDbo: MongoDBList = containerDbo.expand[BasicDBList]("theListWhichNeedsToBeTested").get
-//
-//      val theList: List[SomeCommonTrait] = containerObj.theListWhichNeedsToBeTested
-//      theList.size must_== listDbo.size
-//
-//      for( (nestedObj, i) <- theList.zipWithIndex ) {
-//        val nestedDbo: MongoDBObject = listDbo(i).asInstanceOf[BasicDBObject]
-//        nestedObj match {
-//          case someSubclass: SomeSubclassExtendingSaidTrait => {
-//            println("%s must equal %s".format(someSubclass.b, nestedDbo("b")))
-//            someSubclass.b must_== nestedDbo("b")
-//          }
-//          case anotherSubClass: AnotherSubclassExtendingSaidTrait => {
-//            anotherSubClass.c must_== nestedDbo("c")
-//          }
-//        }
-//      }
-//
-//      // dbo has been verified to have been properly serialized, now, let's see if we can deserialize the serialized obj
-//
-//      val obj_* = grater[SomeTopLevelClass].asObject(dbo)
-//
-//      println(obj_*)
-//      obj must_== obj_*
-//
-//    }
-
     "properly deserialize a raw DBObject" in {
 
-      val listBuilder = MongoDBList.newBuilder
+    // TODO: create an issue for this?
+    // if you comment this block out, the second block fails with
+    // no grater found for 'com.novus.salat.test.model.SomeCommonTrait' OR 'com.novus.salat.test.model.SomeSubclassExtendingSaidTrait' (Injectors.scala:166)
+      {
+        val obj = SomeContainerClass("1",
+          List(SomeSubclassExtendingSaidTrait(2),
+            AnotherSubclassExtendingSaidTrait(3.0),
+            SomeSubclassExtendingSaidTrait(4),
+            AnotherSubclassExtendingSaidTrait(5.0)))
 
-      listBuilder += {
-        val builder = MongoDBObject.newBuilder
-        builder += "_typeHint" -> "com.novus.salat.test.model.SomeSubclassExtendingSaidTrait"
-        builder += "b" -> 2
-        builder.result
+        val dbo: MongoDBObject = grater[SomeContainerClass].asDBObject(obj)
+
+        val obj_* = grater[SomeContainerClass].asObject(dbo)
+
+        obj must_== obj_*
+
       }
 
-      listBuilder += {
-        val builder = MongoDBObject.newBuilder
-        builder += "_typeHint" -> "com.novus.salat.test.model.AnotherSubclassExtendingSaidTrait"
-        builder += "c" -> 3.0
-        builder.result
+
+      {
+
+        val listBuilder = MongoDBList.newBuilder
+
+        listBuilder += {
+          val builder = MongoDBObject.newBuilder
+          builder += "_typeHint" -> "com.novus.salat.test.model.SomeSubclassExtendingSaidTrait"
+          builder += "b" -> 2
+          builder.result
+        }
+
+        listBuilder += {
+          val builder = MongoDBObject.newBuilder
+          builder += "_typeHint" -> "com.novus.salat.test.model.AnotherSubclassExtendingSaidTrait"
+          builder += "d" -> 3.0
+          builder.result
+        }
+
+        val containerClassBuilder = MongoDBObject.newBuilder
+        containerClassBuilder += "_typeHint" -> "com.novus.salat.test.model.SomeContainerClass"
+        containerClassBuilder += "e" -> "some value for e"
+        containerClassBuilder += "theListWhichNeedsToBeTested" -> listBuilder.result
+
+        val dbo = containerClassBuilder.result
+
+        val obj = grater[SomeContainerClass].asObject(dbo)
+
       }
-      //com.novus.salat.test.model.SomeSubclassExtendingSaidTrait
-      //com.novus.salat.test.model.AnotherSubclassExtendingSaidTrait
 
-      val containerClassBuilder = MongoDBObject.newBuilder
-      containerClassBuilder += "_typeHint" -> "com.novus.salat.test.model.SomeContainerClass"
-      containerClassBuilder += "e" -> "some value for e"
-      containerClassBuilder += "theListWhichNeedsToBeTested" -> listBuilder.result
+    }
 
-      val dbo = containerClassBuilder.result
-
-      println(dbo)
-
-      val obj = grater[SomeContainerClass].asObject(dbo)
-
-    } pendingUntilFixed
   }
 
 }
