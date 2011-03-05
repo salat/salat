c515fcc8177e4f344741292723648b438852b6e1
Max Afonov <max@bumnetworks.com>
2011-03-05 00:31:38 -0500

    skip 0.0.6 due to epic subproject split

--- project/build/SalatProject.scala
+++ project/build/SalatProject.scala
@@ -1,30 +1,36 @@
 import sbt._
 
-class SalatProject(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish {
+class SalatProject(info: ProjectInfo) extends ParentProject(info) with posterous.Publish {
+  //override def defaultModuleSettings = inlineSettings
   override def managedStyle = ManagedStyle.Maven
 
-  override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
+  lazy val core = project("salat-core", "salat-core", new SalatCoreProject(_))
 
-  val mongodb = "org.mongodb" % "mongo-java-driver" % "2.4" withSources()
-  val casbah_core = "com.mongodb.casbah" %% "casbah-core" % "2.0.1" withSources()
+  abstract class BaseSalatProject(info: ProjectInfo) extends DefaultProject(info) {
+    override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
 
-  // Should be crossScalaVersionString, but 2.8.0's scalap appears to
-  // be totally frakked, whereas 2.8.1's works fine with 2.8.0. Go
-  // figure.
-  val scalap = "org.scala-lang" % "scalap" % "2.8.1" withSources()
+    val specsVersion = crossScalaVersionString match {
+      case "2.8.0" => "1.6.5"
+      case "2.8.1" => "1.6.7"
+    }
+    val specs = "org.scala-tools.testing" %% "specs" % specsVersion % "test->default" withSources()
 
-  val specsVersion = crossScalaVersionString match {
-    case "2.8.0" => "1.6.5"
-    case "2.8.1" => "1.6.7"
+    val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default" withSources()
+    val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default" withSources()
+
+    override def packageSrcJar= defaultJarPath("-sources.jar")
+    override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
   }
-  val specs = "org.scala-tools.testing" %% "specs" % specsVersion % "test->default" withSources()
 
-  val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default" withSources()
-  val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default" withSources()
+  class SalatCoreProject(info: ProjectInfo) extends BaseSalatProject(info) {
+    val mongodb = "org.mongodb" % "mongo-java-driver" % "2.4" withSources()
+    val casbah_core = "com.mongodb.casbah" %% "casbah-core" % "2.0.3" withSources()
 
-  override def packageSrcJar= defaultJarPath("-sources.jar")
-  val sourceArtifact = Artifact.sources(artifactID)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+    // Should be crossScalaVersionString, but 2.8.0's scalap appears to
+    // be totally frakked, whereas 2.8.1's works fine with 2.8.0. Go
+    // figure.
+    val scalap = "org.scala-lang" % "scalap" % "2.8.1" withSources()
+  }
 
   val publishTo = Resolver.sftp("repo.novus.com", "repo.novus.com", "/nv/repo/%s".format(
     if (projectVersion.value.toString.endsWith("-SNAPSHOT")) "snapshots"
