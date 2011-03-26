907beda0ef10a3e0ba55573b50bbbc47c5673a27
rktoomey <rktoomey@gmail.com>
2011-03-25 20:03:28 -0400

    Updated specs2 dependency to 1.1-SNAPSHOT.

--- project/build/SalatProject.scala
+++ project/build/SalatProject.scala
@@ -13,7 +13,7 @@ class SalatProject(info: ProjectInfo) extends ParentProject(info) with posterous
   abstract class BaseSalatProject(info: ProjectInfo) extends DefaultProject(info) {
     override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
 
-    val specs2 = "org.specs2" %% "specs2" % "1.0.1" % "test->default"
+    val specs2 = "org.specs2" %% "specs2" % "1.1-SNAPSHOT" % "test" withSources()
     val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default" withSources()
     val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default" withSources()
 
