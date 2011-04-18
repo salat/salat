0071cf6439f0e46f1c33f894066ee0e0a85383e3
rktoomey <rktoomey@gmail.com>
2011-04-18 12:56:18 -0400

    Added enclosing class as explanation for missing grater

--- salat-core/src/main/scala/com/novus/salat/util.scala
+++ salat-core/src/main/scala/com/novus/salat/util.scala
@@ -63,6 +63,7 @@ package object util {
 
     def getReasonForPathClazz(pathClazz: Option[Class[_]], path: String): String = {
       pathClazz match {
+        case Some(clazz) if clazz.getEnclosingClass != null => "Class %s has enclosing class %s".format(clazz.getName, clazz.getEnclosingClass.getName)
         case Some(clazz) if clazz.isInstanceOf[CaseClass] => "Well, this is a case class, so... not sure what went wrong."
         case Some(clazz) if clazz.isInterface => "Class %s is an interface".format(clazz.getName)
         case Some(clazz) if Modifier.isAbstract(clazz.getModifiers) => "Class %s is abstract".format(clazz.getName)
@@ -98,6 +99,7 @@ package object util {
       val hintClazz = clazzFromTypeHint(typeHint)(ctx)
       val pathClazz = clazzFromPath(path)
       val reason = hintClazz match {
+        case Some(clazz) if clazz.getEnclosingClass != null => "Class %s has enclosing class %s".format(clazz.getName, clazz.getEnclosingClass.getName)
         case Some(clazz) if clazz.isInstanceOf[CaseClass] => "Well, this is a case class, so... not sure what went wrong."
         case Some(clazz) if clazz.isInterface => "Class %s is an interface".format(clazz.getName)
         case Some(clazz) if Modifier.isAbstract(clazz.getModifiers) => "Class %s is abstract".format(clazz.getName)
