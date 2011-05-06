7d829c5f53204fde374d173513bfdc3e33875e06
rktoomey <rktoomey@gmail.com>
2011-05-06 14:46:09 -0400

    Nascent SalatDAO utils - a helper method for requiring one and only one result

---
+++ salat-core/src/main/scala/com/novus/salat/dao/SalatDAOUtils.scala
@@ -0,0 +1,36 @@
+/**
+* Copyright (c) 2010, 2011 Novus Partners, Inc. <http://novus.com>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* For questions and comments about this product, please see the project page at:
+*
+* http://github.com/novus/salat
+*
+*/
+package com.novus.salat.dao
+
+object SalatDAOUtils {
+
+  /**
+    * Return one or blow up.  You could just use the head method, but the value added here is an error message explaining
+    * how what you thought was a unique selection criteria is... not.  Using head would mask this error, assuming you care.
+   */
+  def exactlyOne[Z](list: List[Z]): Z = list match {
+    case head :: Nil => head
+    case head :: tail => throw new Error("exactlyOne: expected exactly one result but found %d items in the result list:\n%s\n".
+      format(list.size, list.mkString("\n")))   // how long is the list?  that depends on how badly things went wrong back in your collection....
+    case Nil => throw new Error("exactlyOne: expected exactly one but found an empty result list!")
+  }
+
+}
