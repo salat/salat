64af1c7ceef8678fd796f8168fc2d27d70d6b2a0
rktoomey <rktoomey@gmail.com>
2011-06-19 19:41:53 -0400

    create salat-util module for code dependent only on scalap

--- project/build/SalatProject.scala
+++ project/build/SalatProject.scala
@@ -5,16 +5,16 @@ class SalatProject(info: ProjectInfo) extends ParentProject(info) with posterous
   //override def defaultModuleSettings = inlineSettings
   override def managedStyle = ManagedStyle.Maven
 
-  lazy val core = project("salat-core", "salat-core", new SalatCoreProject(_))
-  lazy val proto = project("salat-proto", "salat-proto", new SalatProtoProject(_), core)
+  lazy val util = project("salat-util", "salat-util", new SalatCoreProject(_))
+  lazy val core = project("salat-core", "salat-core", new SalatCoreProject(_), util)
+//  lazy val proto = project("salat-proto", "salat-proto", new SalatProtoProject(_), core)
 
-  val allSource: PathFinder = core.mainSourcePath ** "*.scala" +++
-                              proto.mainSourcePath ** "*.scala"
+  val allSource: PathFinder = util.mainSourcePath ** "*.scala" +++ core.mainSourcePath ** "*.scala"
 
   abstract class BaseSalatProject(info: ProjectInfo) extends DefaultProject(info) {
     override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
 
-    val specs2 = "org.specs2" %% "specs2" % "1.3" % "test"
+    val specs2 = "org.specs2" %% "specs2" % "1.4" % "test"
     val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default"
     val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.0" % "test->default"
 
@@ -29,61 +29,17 @@ class SalatProject(info: ProjectInfo) extends ParentProject(info) with posterous
     override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
   }
 
-  class SalatCoreProject(info: ProjectInfo) extends BaseSalatProject(info) {
+  class SalatUtilProject(info: ProjectInfo) extends BaseSalatProject(info) {
+    val scalap = "org.scala-lang" % "scalap" % crossScalaVersionString
+  }
+
+  class SalatCoreProject(info: ProjectInfo) extends SalatUtilProject(info) {
 
     val mongodb = "org.mongodb" % "mongo-java-driver" % "2.5.3"
     val casbah_core = "com.mongodb.casbah" %% "casbah-core" % "2.1.5.0"
     val commons_pool = "commons-pool" % "commons-pool" % "1.5.5"
 
-    val scalap = "org.scala-lang" % "scalap" % crossScalaVersionString
   }
-
-  class SalatProtoProject(info: ProjectInfo) extends SalatCoreProject(info) with protobuf.ProtobufCompiler {
-    val protobuf = "com.google.protobuf" % "protobuf-java" % "2.3.0"
-
-    override def protobufDirectory = "src" / "test" / "protobuf"
-
-    override def protobufOutputPath = "src" / "test" / "java"
-
-//    override def generateProtobufAction = task {
-//      val mostRecentSchemaTimestamp = protobufSchemas.get.map {
-//        _.asFile.lastModified
-//      }.toList.sort {
-//        _ > _
-//      }.head
-//      log.info("mostRecentSchemaTimestamp: %s".format(mostRecentSchemaTimestamp))
-//      log.info("protobufOutputPath.asFile.lastModified=%s".format(protobufOutputPath.asFile.lastModified))
-//
-//      if (mostRecentSchemaTimestamp >= protobufOutputPath.asFile.lastModified) {
-//        for (schema <- protobufSchemas.get) {
-//          log.info("Compiling schema %s".format(schema))
-//        }
-//        protobufOutputPath.asFile.mkdirs()
-//        log.info("protobufOutputPath: %s".format(protobufOutputPath))
-//        val incPath = protobufIncludePath.map(_.absolutePath).mkString("-I ", " -I ", "")
-//        log.info("incPath: %s".format(incPath))
-//        log.info("""ABOUT TO RUN:
-//        protoc %s --java_out=%s%s
-//        """.format(incPath, protobufOutputPath.absolutePath, protobufSchemas.getPaths.mkString(" ")))
-//        <x>protoc {incPath} --java_out={protobufOutputPath.absolutePath} {protobufSchemas.getPaths.mkString(" ")}</x> ! log
-//        protobufOutputPath.asFile.setLastModified(mostRecentSchemaTimestamp)
-//      }
-//
-//      None
-//    } describedAs ("Generates Java classes from the specified Protobuf schema files.")
-
-    override protected def testCompileAction = {
-//      log.info("testCompileAction: preparing to generate...")
-      super.testCompileAction dependsOn (generateProtobuf)
-    }
-
-    // I only generate protobuf messages for testing, so we don't need to hook this in to compile
-    override def compileAction = super.compileAction
-
-//    override def cleanAction = super.cleanAction dependsOn(cleanProtobuf)
-  }
-
-
 
   val publishTo = Resolver.sftp("repo.novus.com", "repo.novus.com", "/nv/repo/%s".format(
     if (projectVersion.value.toString.endsWith("-SNAPSHOT")) "snapshots"
