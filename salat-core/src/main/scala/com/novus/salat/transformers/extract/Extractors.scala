8154aee295569cc01b05331e30a2a48207a2a0be
rktoomey <rktoomey@gmail.com>
2011-08-02 12:30:49 -0400

    or not.

--- salat-core/src/main/scala/com/novus/salat/transformers/extract/Extractors.scala
+++ salat-core/src/main/scala/com/novus/salat/transformers/extract/Extractors.scala
@@ -172,7 +172,6 @@ trait BigIntToByteArray extends Transformer {
   self: Transformer =>
   override def transform(value: Any)(implicit ctx: Context): Any = value match {
     case bi: BigInt => bi.toByteArray
-    case bi: scala.math.BigInt => bi.toByteArray
     case bi: java.math.BigInteger => bi.toByteArray
     case l: Long => BigInt(l).toByteArray
     case i: Int => BigInt(i).toByteArray
