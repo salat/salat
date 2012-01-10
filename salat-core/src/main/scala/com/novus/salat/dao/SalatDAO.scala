ccd2fab31e060df78ce894da59deafd883cc8408
rktoomey <rktoomey@gmail.com>
2012-01-10 17:33:30 -0500

    Per recommendation from Scott Hernandez, switch SalatDAO from using WriteResult getLastError(WriteConcern concern) method to using getCachedLastError method

--- salat-core/src/main/scala/com/novus/salat/dao/SalatDAO.scala
+++ salat-core/src/main/scala/com/novus/salat/dao/SalatDAO.scala
@@ -199,7 +199,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
     val _id = try {
       val dbo = _grater.asDBObject(t)
       val wr = collection.insert(dbo, wc)
-      if (wr.getLastError(wc).ok()) {
+      if (wr.getCachedLastError == null) {
         dbo.getAs[ID]("_id")
       }
       else {
@@ -214,7 +214,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
     val _ids = try {
       val dbos = docs.map(t => _grater.asDBObject(t))
       val wr = collection.insert(dbos, wc)
-      if (wr.getLastError(wc).ok()) {
+      if (wr.getCachedLastError == null) {
         val builder = List.newBuilder[Option[ID]]
         for (dbo <- dbos) {
           builder += dbo.getAs[ID]("_id") orElse {
@@ -251,8 +251,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
     try {
       val dbo = _grater.asDBObject(t)
       val wr = collection.remove(dbo, wc)
-      val cr = wr.getLastError(wc)
-      if (!cr.ok()) {
+      if (wr.getCachedLastError != null) {
         throw SalatRemoveError(description, collection, wc, wr, List(dbo))
       }
     }
@@ -265,8 +264,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
   def remove[A <% DBObject](q: A, wc: WriteConcern) {
     try {
       val wr = collection.remove(q, wc)
-      val cr = wr.getLastError(wc)
-      if (!cr.ok()) {
+      if (wr.getCachedLastError != null) {
         throw SalatRemoveQueryError(description, collection, q, wc, wr)
       }
     }
@@ -288,8 +286,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
     try {
       val dbo = _grater.asDBObject(t)
       val wr = collection.save(dbo, wc)
-      val cr = wr.getLastError(wc)
-      if (!cr.ok()) {
+      if (wr.getCachedLastError != null) {
         throw SalatSaveError(description, collection, wc, wr, List(dbo))
       }
     }
@@ -298,8 +295,7 @@ abstract class SalatDAO[ObjectType <: CaseClass, ID <: Any](val collection: Mong
   def update[A <% DBObject, B <% DBObject](q: A, o: B, upsert: Boolean = false, multi: Boolean = false, wc: WriteConcern = collection.writeConcern) {
     try {
       val wr = collection.update(q, o, upsert, multi, wc)
-      val cr = wr.getLastError(wc)
-      if (!cr.ok()) {
+      if (wr.getCachedLastError != null) {
         throw SalatDAOUpdateError(description, collection, q, o, wc, wr, upsert, multi)
       }
     }
