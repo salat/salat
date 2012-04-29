be5013063c7e4726e9acac33d3e452e79912d1a1
rktoomey <rktoomey@gmail.com>
2012-04-28 20:54:17 -0400

    Update copyright notices.

--- salat-util/src/main/scala/com/novus/salat/util/Logging.scala
+++ salat-util/src/main/scala/com/novus/salat/util/Logging.scala
@@ -1,27 +1,30 @@
-/** Copyright (c) 2010, 2011 Novus Partners, Inc. <http://novus.com>
+/*
+ * Copyright (c) 2010 - 2012 Novus Partners, Inc. <http://novus.com>
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Module:        salat-util
+ * Class:         Logging.scala
+ * Last modified: 2012-04-28 20:34:21 EDT
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  For questions and comments about this product, please see the project page at:
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  http://github.com/novus/salat
- *
- *  NOTICE: This Logging Class is derived from the Apache License 2.0
- *  Akka Project, available from http://akkasource.org/
- */
-/** Derived from work Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ * Project:      http://github.com/novus/salat
+ * Wiki:         http://github.com/novus/salat/wiki
+ * Mailing list: http://groups.google.com/group/scala-salat
  */
 package com.novus.salat.util
+
+/** Derived from work Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
 
 import org.slf4j.{ Logger => SLFLogger, LoggerFactory => SLFLoggerFactory }
 
--- salat-util/src/test/scala/com/novus/salat/util/SalatDAOUtilSpec.scala
+++ salat-util/src/test/scala/com/novus/salat/util/SalatDAOUtilSpec.scala
@@ -1,26 +1,30 @@
+/*
+ * Copyright (c) 2010 - 2012 Novus Partners, Inc. <http://novus.com>
+ *
+ * Module:        salat-util
+ * Class:         SalatDAOUtilSpec.scala
+ * Last modified: 2012-04-28 20:34:21 EDT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project:      http://github.com/novus/salat
+ * Wiki:         http://github.com/novus/salat/wiki
+ * Mailing list: http://groups.google.com/group/scala-salat
+ */
+
 package com.novus.salat.util
 
-/** Copyright (c) 2010, 2011 Novus Partners, Inc. <http://novus.com>
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  For questions and comments about this product, please see the project page at:
- *
- *  http://github.com/novus/salat
- *
- */
 import org.specs2.mutable.Specification
-import com.novus.salat.util.SalatDAOUtils
 
 class SalatDAOUtilSpec extends Specification {
 
