a3f8f78275b655bfb914bdd861bae4d8cfdbe6a6
rktoomey <rktoomey@gmail.com>
2012-12-06 22:10:42 -0500

    #57: jury rig build file for Scala 2.10.0-RC3

--- project/SalatBuild.scala
+++ project/SalatBuild.scala
@@ -3,7 +3,7 @@
  *
  * Module:        salat-build
  * Class:         SalatBuild.scala
- * Last modified: 2012-10-15 20:46:51 EDT
+ * Last modified: 2012-12-06 21:59:13 EST
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ object BuildSettings {
 
   val buildOrganization = "com.novus"
   val buildVersion = "1.9.2-SNAPSHOT"
-  val buildScalaVersion = "2.9.2"
+  val buildScalaVersion = "2.10.0-RC3"
 
   val buildSettings = Defaults.defaultSettings ++ Format.settings ++ Publish.settings ++ Ls.settings ++ net.virtualvoid.sbt.graph.Plugin.graphSettings ++ Seq(
     organization := buildOrganization,
@@ -79,7 +79,7 @@ object BuildSettings {
     testFrameworks += TestFrameworks.Specs2,
     resolvers ++= Seq(typeSafeRepo, typeSafeSnapsRepo, oss, ossSnaps),
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
-    crossScalaVersions := Seq("2.9.1", "2.9.2")
+    crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.0-RC3")
   )
 }
 
@@ -146,13 +146,13 @@ object Dependencies {
 
   private val LogbackVersion = "1.0.7"
 
-  val specs2 = "org.specs2" %% "specs2" % "1.12.2" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.13-SNAPSHOT" % "test"
   val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.4"
   val logbackCore = "ch.qos.logback" % "logback-core" % LogbackVersion % "test"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % LogbackVersion % "test"
-  val casbah = "org.mongodb" %% "casbah" % "2.4.1" pomOnly()
-  val json4sNative = "org.json4s" %% "json4s-native" % "3.0.0"
+  val casbah = "com.github.tmingos" % "casbah_2.10" % "2.5.0-SNAPSHOT" pomOnly()
+  val json4sNative = "org.json4s" % "json4s-native_2.10.0-RC3" % "3.1.0-SNAPSHOT"
 }
 
 object Repos {
