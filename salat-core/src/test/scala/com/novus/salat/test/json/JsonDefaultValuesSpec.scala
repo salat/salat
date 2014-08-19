ab780f1091ac813c24b82f2fee81b4db0cae917d
Alex Wajda <alexander.wajda@gmail.com>
2014-08-19 23:10:14 +0200

    Issue #130 - Default constructor argument deserialization from JSON. Adding failing unit test.

--- salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
+++ salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2010 - 2013 Novus Partners, Inc. (http://www.novus.com)
- *
- * Module:        salat-core
- * Class:         JsonSpec.scala
- * Last modified: 2013-02-25 21:07:26 EST
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *           Project:  http://github.com/novus/salat
- *              Wiki:  http://github.com/novus/salat/wiki
- *      Mailing list:  http://groups.google.com/group/scala-salat
- *     StackOverflow:  http://stackoverflow.com/questions/tagged/salat
- */
 package com.novus.salat.test.json
 
 import com.novus.salat._
@@ -30,6 +6,9 @@ import org.bson.types.ObjectId
 import org.specs2.matcher.JsonMatchers
 import org.specs2.mutable.Specification
 
+/**
+ * @see https://github.com/novus/salat/issues/130
+ */
 class JsonDefaultValuesSpec extends Specification with Logging with JsonMatchers {
 
   "JSON support" should {
