d577682ff06e4172b4ed5f4997bd6bc7d7fbba53
rktoomey <rktoomey@gmail.com>
2014-08-19 22:14:44 -0400

    2.0.0-SNAPSHOT

--- project/SalatBuild.scala
+++ project/SalatBuild.scala
@@ -69,7 +69,7 @@ object BuildSettings {
   import Repos._
 
   val buildOrganization = "com.novus"
-  val buildVersion = "1.9.9"
+  val buildVersion = "2.0.0-SNAPSHOT"
   val buildScalaVersion = "2.11.2"
 
   val buildSettings = Defaults.defaultSettings ++ Format.settings ++ Publish.settings ++ Seq(
