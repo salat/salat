4798d40f51aaa4dcaa9064da9475654a6fec2783
Noah Zucker <nzucker@novus.com>
2014-10-08 18:47:54 -0400

    Unit test exposing issue #137

--- salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
+++ salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
@@ -10,7 +10,7 @@ import org.specs2.mutable.Specification
  */
 class JsonDefaultValuesSpec extends Specification with Logging with JsonMatchers {
 
-  "JSON support" should {
+  "JSON support with default arg suppression disabled (the default)" should {
     "evaluate default constructor argument when corresponding JSON property is missing" in {
 
       val deserializedObj1 = grater[Probe].fromJSON("{}")
@@ -20,6 +20,20 @@ class JsonDefaultValuesSpec extends Specification with Logging with JsonMatchers
       deserializedObj1.id should not be deserializedObj2.id
     }
   }
+
+  "JSON support with default arg suppression enabled" should {
+    implicit val ctx = new Context {
+      val name = "suppressDefault"
+      override val suppressDefaultArgs = true
+    }
+    "serialize instances with multiple ObjectId fields that do not have default values" in {
+      val father = Probe()
+      val child = ChildProbe(id = new ObjectId, parent = father.id)
+      val result = grater[ChildProbe].toCompactJSON(child)
+      result must not(throwA[NoSuchMethodException]) and beAnInstanceOf[String]
+    }
+  }
 }
 
 case class Probe(id: ObjectId = new ObjectId())
+case class ChildProbe(id: ObjectId, parent: ObjectId)
