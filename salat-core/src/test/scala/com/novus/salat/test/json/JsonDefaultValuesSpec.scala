2410ee9b75a72680a6bfa58406be79ef86e7b4de
Noah Zucker <nzucker@novus.com>
2014-10-28 16:35:49 -0400

    Issuer #137 - Fix. Also, minor refactoring of surrounding (redundant) code.

--- salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
+++ salat-core/src/test/scala/com/novus/salat/test/json/JsonDefaultValuesSpec.scala
@@ -20,6 +20,32 @@ class JsonDefaultValuesSpec extends Specification with Logging with JsonMatchers
       deserializedObj1.id should not be deserializedObj2.id
     }
   }
+
+  "JSON support with default arg suppression enabled" should {
+    implicit val ctx = new Context {
+      val name = "suppressDefault"
+      override val suppressDefaultArgs = true
+    }
+    "serialize instances with multiple ObjectId fields that do not have default values" in {
+      val father = Probe()
+      val child = ChildProbe(id = new ObjectId, parent = father.id)
+      val result = grater[ChildProbe].toCompactJSON(child)
+      result must not(throwA[NoSuchMethodException]) and beAnInstanceOf[String]
+    }
+    "serialize instances with Optional ObjectId fields that do not have default values" in {
+      val child = ChildProbeParentOptional(id = new ObjectId, parent = None)
+      val result = grater[ChildProbeParentOptional].toCompactJSON(child)
+      result must not(throwA[NoSuchMethodException]) and beAnInstanceOf[String]
+    }
+    "serialize instances with lists of ObjectIds that do not have default values" in {
+      val child = ChildProbeManyParents(id = new ObjectId, parents = Nil)
+      val result = grater[ChildProbeManyParents].toCompactJSON(child)
+      result must not(throwA[NoSuchMethodException]) and beAnInstanceOf[String]
+    }
+  }
 }
 
 case class Probe(id: ObjectId = new ObjectId())
+case class ChildProbe(id: ObjectId, parent: ObjectId)
+case class ChildProbeParentOptional(id: ObjectId, parent: Option[ObjectId])
+case class ChildProbeManyParents(id: ObjectId, parents: List[ObjectId])
