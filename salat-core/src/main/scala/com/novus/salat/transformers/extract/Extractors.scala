adfdd7b3a2dee4922db766646e29b10425fbb045
noahlz <nzucker@gmail.com>
2016-07-11 00:29:51 -0400

    Updated copyright using IntelliJ copyright plugin and copywrite text from e6f19c009047e6851447daf79b925ed1e1da22dc
    See: https://www.jetbrains.com/help/idea/2016.1/generating-and-updating-copyright-notice.html

--- salat-core/src/main/scala/com/novus/salat/transformers/extract/Extractors.scala
+++ salat-core/src/main/scala/com/novus/salat/transformers/extract/Extractors.scala
@@ -1,9 +1,10 @@
 /*
- * Copyright (c) 2010 - 2013 Novus Partners, Inc. (http://www.novus.com)
+ * Copyright (c) 2010 - 2015 Novus Partners, Inc. (http://www.novus.com)
+ * Copyright (c) 2015 - 2016 Rose Toomey (https://github.com/rktoomey) and other individual contributors where noted
  *
  * Module:        salat-core
  * Class:         Extractors.scala
- * Last modified: 2013-01-07 22:59:22 EST
+ * Last modified: 2016-07-10 23:49:08 EDT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +18,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *           Project:  http://github.com/novus/salat
- *              Wiki:  http://github.com/novus/salat/wiki
+ *           Project:  http://github.com/salat/salat
+ *              Wiki:  http://github.com/salat/salat/wiki
+ *             Slack:  https://scala-salat.slack.com
  *      Mailing list:  http://groups.google.com/group/scala-salat
  *     StackOverflow:  http://stackoverflow.com/questions/tagged/salat
+ *
  */
 package com.novus.salat.transformers
 
