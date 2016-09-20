762e6506021b9afbc1db97ba549c6627c417b8a5
noahlz <nzucker@gmail.com>
2016-09-19 23:48:34 -0400

    Post-release. Next version is 1.10.1

--- project/SalatBuild.scala
+++ project/SalatBuild.scala
@@ -66,7 +66,7 @@ object BuildSettings {
   import Repos._
 
   val buildOrganization = "com.github.salat"
-  val buildVersion = "1.10.0"
+  val buildVersion = "1.10.1-SNAPSHOT"
   val buildScalaVersion = "2.11.7"
 
   val buildSettings = Defaults.coreDefaultSettings ++ Scalariform.settings ++ Publish.settings ++ Seq(
